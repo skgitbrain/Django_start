81716ab3c71ca60f853e8514da9f152e31e8a1e7
skbrain <gitskbrain@bk.ru>
2021-09-03 14:42:44 +0300

    update lesson_2

--- geekshop/mainapp/views.py
+++ geekshop/mainapp/views.py
@@ -1,13 +1,17 @@
 from django.shortcuts import render
 
 def main(request):
-    return render(request, 'mainapp/index.html')
+    context = {'page_title': 'магазин'}
+    return render(request, 'mainapp/index.html', context)
 
 def products(request):
-    return render(request, 'mainapp/products.html')
+    products_menu = ['все', 'дом','офис', 'модерн', 'классика']
+    context = {'page_title': 'каталог', 'products_menu': products_menu}
+    return render(request, 'mainapp/products.html', context)
 
 def contact(request):
-    return render(request, 'mainapp/contact.html')
+    context = {'page_title': 'контакты'}
+    return render(request, 'mainapp/contact.html', context)
 
 def temp(request):
     return render(request, 'mainapp/temp1.html')
